cadeb39eb0bb48c732071a2dda58105aa932226a
maraja <amit.maraj@gmail.com>
2018-08-22 13:47:03 -0400

    first commit

---
+++ webpack/webpack.common.js
@@ -0,0 +1,63 @@
+const webpack = require('webpack');
+const commonPaths = require('./paths');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: commonPaths.entryPath,
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        loader: 'eslint-loader',
+        exclude: /(node_modules)/,
+        options: {
+          emitWarning: process.env.NODE_ENV !== 'production',
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
+      },{
+        test: /\.mdx$/,
+        ignore: /node_modules/,
+        use: [
+          'babel-loader',
+          'mdx-deck/loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: commonPaths.imagesFolder,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff2|ttf|woff|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: commonPaths.fontsFolder,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: commonPaths.templatePath,
+    }),
+  ],
+};
